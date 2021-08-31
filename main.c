6e01ff3e81b37d422a2745689374c82751fcd720
Lorenzo Di Gaetano <lorenzodigaetano@yahoo.it>
2021-09-01 00:01:24 +0200

    Ok load_asset

--- main.c
+++ main.c
@@ -243,6 +243,8 @@ int main(int argc, char **argv)
 
     UBYTE *bitplanes = init_bitplanes(GRAPHICS_BPLS_SIZE);
 
+    load_asset(bitplanes,GRAPHICS_BPLS_SIZE,"Pic.raw");
+
     point_bitplanes(bitplanes,&copperlist[BPL1PTH_VALUE_IDX],5);
 
     /*
