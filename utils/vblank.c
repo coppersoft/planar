64993222af9c909f7c4429b7fe75ab29f6ae784c
Lorenzo Di Gaetano <lorenzodigaetano@yahoo.it>
2021-09-07 15:46:39 +0200

    Messo il vblank del cinese

--- utils/vblank.c
+++ utils/vblank.c
@@ -1,15 +1,13 @@
 #include "vblank.h"
 
-//#define RAST_LINE_TO_WAIT   (0xc1)
-#define RAST_LINE_TO_WAIT   (0x2a)
+#define RAST_LINE_TO_WAIT   (0xc1)      // ok Amiga 1200
+//#define RAST_LINE_TO_WAIT   (0x2a)    // ok Amiga 500
 
-// 303 è PAL, se NTSC 262
-/*void wait_vblank()
-{
-    while (((*custom_vposr) & 0x1ff00) != (303<<8)) ;
-}*/
 
-void wait_vblank() {
+/*
+    Versione mia presa da Fabio Ciucci
+*/
+/*void wait_vblank() {
 
     volatile UBYTE *custom_vposr_low = (volatile UBYTE *) 0xdff005;
     volatile UBYTE *custom_vhposr = (volatile UBYTE *) 0xdff006;
@@ -19,4 +17,15 @@ void wait_vblank() {
     while ((*custom_vhposr) != RAST_LINE_TO_WAIT);
 
     while ((*custom_vhposr) == RAST_LINE_TO_WAIT);
+}*/
+
+/*
+    Versione del corso del cinese.
+*/
+static volatile ULONG *custom_vposr = (volatile ULONG *) 0xdff004;
+static vb_waitpos;
+
+void wait_vblank() {
+    vb_waitpos = 303;   // PAL
+    while (((*custom_vposr) & 0x1ff00) != (vb_waitpos<<8)) ;
 }
